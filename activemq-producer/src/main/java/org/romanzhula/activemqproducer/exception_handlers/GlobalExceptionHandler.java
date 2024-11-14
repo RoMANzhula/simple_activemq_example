586488ef6cc2289eb7a73b4a541829ee8911a8f9
RoMANzhula <roman1985al@ukr.net>
2024-11-14 22:56:11 +0200

    Add GlobalExceptionHandler - producer module

---
+++ activemq-producer/src/main/java/org/romanzhula/activemqproducer/exception_handlers/GlobalExceptionHandler.java
@@ -0,0 +1,28 @@
+package org.romanzhula.activemqproducer.exception_handlers;
+
+import org.romanzhula.activemqproducer.exceptions.InvalidMessageException;
+import org.romanzhula.activemqproducer.exceptions.InvalidMessageTypeException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<String> handleException(Exception e) {
+        return new ResponseEntity<>("An Error occurred: " + e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @ExceptionHandler(InvalidMessageException.class)
+    public ResponseEntity<String> handleInvalidMessageException(InvalidMessageException e) {
+        return new ResponseEntity<>("The message is incorrect: " + e.getMessage(), HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(InvalidMessageTypeException.class)
+    public ResponseEntity<String> handleInvalidMessageTypeException(InvalidMessageTypeException e) {
+        return new ResponseEntity<>("Invalid type of message: " + e.getMessage(), HttpStatus.BAD_REQUEST);
+    }
+
+}
