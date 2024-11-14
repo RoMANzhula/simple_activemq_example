f12a059fc1b0a1a302656b935ef677e678e6a82c
RoMANzhula <roman1985al@ukr.net>
2024-11-14 22:55:15 +0200

    Add InvalidMessageTypeException - producer module

---
+++ activemq-producer/src/main/java/org/romanzhula/activemqproducer/exceptions/InvalidMessageTypeException.java
@@ -0,0 +1,8 @@
+package org.romanzhula.activemqproducer.exceptions;
+
+public class InvalidMessageTypeException extends RuntimeException {
+
+    public InvalidMessageTypeException(String message) {
+        super(message);
+    }
+}
