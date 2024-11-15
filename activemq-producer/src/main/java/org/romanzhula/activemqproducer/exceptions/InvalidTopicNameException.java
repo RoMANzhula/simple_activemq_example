80de41d00b33eacad3503f5ce885ea6d0af3101c
RoMANzhula <roman1985al@ukr.net>
2024-11-15 15:38:40 +0200

    Add InvalidTopicNameException - producer module

---
+++ activemq-producer/src/main/java/org/romanzhula/activemqproducer/exceptions/InvalidTopicNameException.java
@@ -0,0 +1,9 @@
+package org.romanzhula.activemqproducer.exceptions;
+
+public class InvalidTopicNameException extends RuntimeException {
+
+    public InvalidTopicNameException(String message) {
+        super(message);
+    }
+
+}
